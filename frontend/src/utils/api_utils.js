7157c7aa59e775dc0cdd5b01c561f8aa4ea0abde
tarikyayla <tarik.yayla33@gmail.com>
2020-10-17 16:05:08 +0300

    CSRF-Token and ApiUtils created

---
+++ frontend/src/utils/api_utils.js
@@ -0,0 +1,19 @@
+const ApiUtils = {
+    getHeader: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json',
+      'X-CSRFToken': this.getCookie('csrftoken')
+    },
+    getCookie: (name) => {
+            var nameEQ = name + "=";
+            var ca = document.cookie.split(';');
+            for(var i=0;i < ca.length;i++) {
+                var c = ca[i];
+                while (c.charAt(0)==' ') c = c.substring(1,c.length);
+                if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+            }
+            return null;
+        }
+}
+
+export default ApiUtils;
