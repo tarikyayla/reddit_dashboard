90e89ab5c8c6bdbc114e1d3f09ee6ca94454f92a
tarikyayla <tarik.yayla33@gmail.com>
2020-10-17 16:06:17 +0300

    url routing for login register app

--- api/urls.py
+++ api/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 from api.views.public import api_check
+from api.views.user import login, register
 
 
 urlpatterns = [
     path("hello/", api_check),
+    path("login/", login),
+    path("register", register)
 ]
